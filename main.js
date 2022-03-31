356263652326dce1662b39f17b2536f895cebcf0
DorianNet93 <lifebsfg@gmail.com>
2022-03-31 17:21:51 +0300

    first cpmmit

---
+++ main.js
@@ -0,0 +1,19 @@
+function tests () {
+    const num = +prompt("введите число");
+
+    if (num % 2 === 0) {
+        alert ("Четное");
+    } else {
+        alert ("Не четное");
+    };
+
+
+    if (num % 1 === 0) {
+        alert ("Целое число");
+    } else {
+        alert ("Не целое число");
+    };    
+};
+
+
+
